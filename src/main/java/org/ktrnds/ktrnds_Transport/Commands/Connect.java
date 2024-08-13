76a59abedb2c66da841208f3f61e9ff6a418aaec
Harukotokoto <n_rui@outlook.jp>
2024-08-13 22:53:26 +0900

    正常に権限が動作しない不具合意を修正

--- src/main/java/org/ktrnds/ktrnds_Transport/Commands/Connect.java
+++ src/main/java/org/ktrnds/ktrnds_Transport/Commands/Connect.java
@@ -14,14 +14,14 @@ import java.util.Objects;
 
 public class Connect implements CommandExecutor {
   public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-    if (sender instanceof Player player) {
-      if (args.length == 0) {
-        sender.sendMessage(ChatColor.RED + "Usage: /connect <Server> [Player]");
-        return true;
-      }
+    if (args.length == 0) {
+      sender.sendMessage(ChatColor.RED + "Usage: /connect <Server> [Player]");
+      return true;
+    }
 
-      if (args.length == 1) {
-        if (sender.hasPermission("bungeecord.server" + args[0].toLowerCase()) || sender.isOp()) {
+    if (args.length == 1) {
+      if (sender instanceof Player player) {
+        if (sender.hasPermission("bungeecord.server." + args[0].toLowerCase()) || sender.isOp()) {
           ByteArrayDataOutput out = ByteStreams.newDataOutput();
           out.writeUTF("Connect");
           out.writeUTF(args[0]);
@@ -29,40 +29,36 @@ public class Connect implements CommandExecutor {
 
           Plugin plugin = Bukkit.getPluginManager().getPlugin("Ktrnds_Transport");
           player.sendPluginMessage(Objects.requireNonNull(plugin), "BungeeCord", data);
-
-          return true;
         } else {
           sender.sendMessage(ChatColor.RED + "You don't have permission to access this server");
         }
       } else {
-        if (sender.hasPermission("ktrnds.transport.connect.other") || sender.isOp()) {
-          Player target = Bukkit.getPlayer(args[1]);
-          if (target != null) {
-            if (target.hasPermission("bungeecord.server" + args[0].toLowerCase()) || target.isOp()) {
-              ByteArrayDataOutput out = ByteStreams.newDataOutput();
-              out.writeUTF("Connect");
-              out.writeUTF(args[0]);
-              byte[] data = out.toByteArray();
-
-              Plugin plugin = Bukkit.getPluginManager().getPlugin("Ktrnds_Transport");
-              target.sendPluginMessage(Objects.requireNonNull(plugin), "BungeeCord", data);
-
-              return true;
-            } else {
-              sender.sendMessage(ChatColor.RED + "User doesn't have permission to access this server");
-            }
-          } else {
-            sender.sendMessage(ChatColor.RED + "Player not found");
-            return true;
-          }
-        } else {
-          sender.sendMessage(ChatColor.RED + "You don't have permission to use this command");
-          return true;
-        }
+        sender.sendMessage("You are not player");
       }
     } else {
-      sender.sendMessage(ChatColor.RED + "You must be a player to use this command.");
+      if (sender.hasPermission("ktrnds.transport.connect.other") || sender.isOp()) {
+        Player target = Bukkit.getPlayer(args[1]);
+        if (target != null) {
+          if (target.hasPermission("bungeecord.server" + args[0].toLowerCase()) || target.isOp()) {
+            ByteArrayDataOutput out = ByteStreams.newDataOutput();
+            out.writeUTF("Connect");
+            out.writeUTF(args[0]);
+            byte[] data = out.toByteArray();
+
+            Plugin plugin = Bukkit.getPluginManager().getPlugin("Ktrnds_Transport");
+            target.sendPluginMessage(Objects.requireNonNull(plugin), "BungeeCord", data);
+
+          } else {
+            sender.sendMessage(ChatColor.RED + "User doesn't have permission to access this server");
+          }
+        } else {
+          sender.sendMessage(ChatColor.RED + "Player not found");
+        }
+      } else {
+        sender.sendMessage(ChatColor.RED + "You don't have permission to use this command");
+      }
     }
+
     return true;
   }
 }
